05f2a05c3edf7a5b2521ced76b9ab8a1a7d85c5a
Kellen Sullivan <142142508+k3113n@users.noreply.github.com>
2024-02-20 22:32:47 -0600

    fixed multiple instances of server connections

--- index.js
+++ index.js
@@ -9,19 +9,28 @@ const isEmpty = (variable) => {
     );
 };
 
+let server = null;
 let relay = new WebSocket.Server({port: 443});
 clients = {};    
+
 relay.on('connection', function (client, req) {
     let user = crypto.randomUUID();
     clients[user] = client;
 
-    let server = new WebSocket(process.env.SERVER);
-    server.on('message', function (message){         
-        client.send(message);
-    });
+    if(!server) {
+        server = new WebSocket(process.env.SERVER);
+
+        server.on('message', function (message){         
+            client.send(message);
+        });
+
+        server.on('close', function() {
+            server = null;
+        });
+    }
 
     client.on('close', function () {
         delete clients[user];
-        if (isEmpty(clients)) server.close();
+        if(isEmpty(clients) && server) server.close();
     });
 });
